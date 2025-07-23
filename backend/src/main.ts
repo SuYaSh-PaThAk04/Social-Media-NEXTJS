7c5d16563f88fa77f8af00713cdf6d11d22f9c05
Suyash Pathak <suyashp271@gmail.com>
2025-07-23 18:19:54 +0530

    Added Frontend Url for dealing with CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // Validates incoming data automatically
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: ['https://social-media-nextjs-pearl.vercel.app/','https://vercel.com/suyash-pathak04s-projects/social-media-nextjs/Cah82CV26xkDdEX35aFm6BRQUvg7'],
     credentials: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
