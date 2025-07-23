85bfaacf6713cf96b14f8059e935dd45b4debc94
Suyash Pathak <suyashp271@gmail.com>
2025-07-23 10:16:38 +0530

    Update next.config.ts

--- cleverbook-frontend/next.config.ts
+++ cleverbook-frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Disable ESLint checks during build
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
