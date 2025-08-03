7059c84cd15627e8c39782d2253d97d68f6af41b
Suyash Pathak <suyashp271@gmail.com>
2025-08-03 18:39:44 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // Validates incoming data automatically
+
+  app.useGlobalPipes(new ValidationPipe());
+
   app.enableCors({
-    origin: 'https://vercel.com/suyash-pathak04s-projects/social-media-nextjs/2sc5TSGEEhXm2G3duDRCAECMWLQG','https://social-media-nextjs-xi.vercel.app,
-    credentials: true, 
+    origin: [
+      'https://vercel.com/suyash-pathak04s-projects/social-media-nextjs/2sc5TSGEEhXm2G3duDRCAECMWLQG',
+      'https://social-media-nextjs-xi.vercel.app',
+    ],
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
+
   await app.listen(3000);
 }
 bootstrap();
